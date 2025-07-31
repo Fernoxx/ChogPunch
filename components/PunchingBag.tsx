ae24077a03430651e0e6d869547b064ecbcc86e2
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-01 02:56:58 +0530

    Update PunchingBag.tsx

--- components/PunchingBag.tsx
+++ components/PunchingBag.tsx
@@ -5,8 +5,6 @@ import clsx from "clsx"
 interface Props {
   anim: ChogAnim
 }
-
-export default function PunchingBag({ anim }: Props) {
   return (
     <div className="absolute right-12 top-20 w-32 h-64">
       <div className={clsx(
