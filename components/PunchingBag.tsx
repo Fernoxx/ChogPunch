c23a93519075b61d52e7493520222f36d07c3f28
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-01 02:57:09 +0530

    Update PunchingBag.tsx

--- components/PunchingBag.tsx
+++ components/PunchingBag.tsx
@@ -1,11 +1,1 @@
-import Image from "next/image"
-import { ChogAnim } from "./Chog"
-import clsx from "clsx"
 
-interface Props {
-  anim: ChogAnim
-}
-  return (
-    <div className="absolute right-12 top-20 w-32 h-64">
-      <div className={clsx(
-        />
