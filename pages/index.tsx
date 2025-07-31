8e4fc24b2cf2ee70ab1718c3fddf0e82d35402b5
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-07-31 16:02:56 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import { useAccount, useWriteContract } from "wagmi"
 import Chog from "@/components/Chog"
 import PunchingBag from "@/components/PunchingBag"
 import Joystick from "@/components/Joystick"
-import chogPunchABI from "@/lib/chogPunchABI.json"
 
 export default function Home() {
   const { address, isConnected } = useAccount()
