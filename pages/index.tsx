d4e9114124025dffb21916b1fcdefcd13022d7df
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-02 03:51:32 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,6 @@ export default function Home() {
           <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-xs text-white">
             built by{" "}
             <a
-              href="https://farcaster.xyz/doteth"
               target="_blank"
             >
               @doteth
