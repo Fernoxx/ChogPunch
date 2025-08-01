560b15bd923b8cddbf69b918e7f2ae07ec71c4da
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-02 03:48:49 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,6 @@ import type { AppProps } from "next/app"
 import { useEffect } from "react"
 import { WagmiConfig, createConfig } from "wagmi"
 import { base } from "wagmi/chains"
-import { InjectedConnector } from "wagmi/connectors/injected"
-    
-    autoConnect: true,
-    publicClient,
-    connectors: [farcasterConnector],
-  })
 
   return (
     <WagmiConfig config={config}>
