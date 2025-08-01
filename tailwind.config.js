936ec2999577e13db19e26662fea5db6e2b17992
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-02 01:37:11 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,6 @@ module.exports = {
         sway:  "sway 2s ease-in-out infinite",
         kick:  "kick 0.5s ease-in-out",
         punch: "punch 0.5s ease-in-out",
-        push:  "push 0.5s ease-in-out"
       }
     },
   },
