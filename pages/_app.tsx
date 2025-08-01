758f964c5aa1ff097f70c5ff688cf2dbfbe1539c
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-02 03:47:05 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,7 +36,6 @@ export default function App({ Component, pageProps }: AppProps) {
   const config = createConfig({
     autoConnect: true,
     publicClient,
-    connectors: [farcasterConnector],
   })
 
   return (
