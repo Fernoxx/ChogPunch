7d4d26ceefdefabfdf38a8a2ed6770fe713a890d
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-04 02:06:08 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import type { AppProps } from "next/app"
 import { useEffect } from "react"
 import { WagmiConfig, createConfig } from "wagmi"
 import { base } from "wagmi/chains"
-import { InjectedConnector } from "wagmi/connectors/injected"
 
   useEffect(() => {
     ;(async () => {
