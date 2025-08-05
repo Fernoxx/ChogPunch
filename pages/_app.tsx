623fe8039c8e0ebcac932a8b2b89179b848aeed8
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-05 22:35:30 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import { useEffect } from "react"
 import { WagmiConfig, createConfig } from "wagmi"
 import { base } from "wagmi/chains"
 import { InjectedConnector } from "wagmi/connectors/injected"
-import { createPublicClient, http } from "viem"
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
