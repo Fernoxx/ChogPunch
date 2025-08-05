601105ae72047c20fc1a1e4aee1e286e2ad23eed
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-05 22:36:57 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { useEffect } from "react"
 import { WagmiConfig, createConfig } from "wagmi"
+
   useEffect(() => {
     ;(async () => {
       try {
