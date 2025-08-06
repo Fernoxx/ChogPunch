730fbf6c92f8f19358b5a8c37ca650194b4c3709
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-07 01:20:07 +0530

    Update AnimationController.ts

--- lib/animation/AnimationController.ts
+++ lib/animation/AnimationController.ts
@@ -120,7 +120,6 @@ export class AnimationController {
     });
 
     // Uppercut
-    this.animations.set('uppercut', {
       name: 'uppercut',
           limbPositions: new Map([
             ['torso', { x: 0, y: 5, rotation: 0.2 }],
