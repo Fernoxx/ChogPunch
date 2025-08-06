746b0fa99fe71693b240ee570699cf6d704747ee
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-07 01:13:56 +0530

    Update AnimationController.ts

--- lib/animation/AnimationController.ts
+++ lib/animation/AnimationController.ts
@@ -126,7 +126,6 @@ export class AnimationController {
           limbPositions: new Map([
             ['leftUpperArm', { x: -20, y: -3, rotation: 1.2 }],
             ['leftLowerArm', { x: -35, y: -3, rotation: 1.5 }],
-            ['torso', { x: -8, y: 0, rotation: 0.3 }],
           ]),
           duration: 120,
           hitboxActive: true,
