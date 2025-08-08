6217c6cf9d1a929b5168893a0325daf593b4f986
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-09 02:39:57 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen relative overflow-hidden">
-      {/* Background */}
-      <ditext-2xl font-bold px-12 py-6 rounded-lg shadow-2xl hover:scale-105 transition-trans
-                START GAME
-              </motion.button>
-            </div>
-          </motion.div>
+     
         )}
       </AnimatePresence>
 
