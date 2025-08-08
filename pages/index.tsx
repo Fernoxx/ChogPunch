9539ec539027efe85ec010d987025a24ed419c00
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-09 02:40:28 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 // pages/index.tsx
 import { useEffect, useState } from "react"
 import { PlatformerGame } from "../components/PlatformerGame"
-export default function Home() {
-  const [stage, setStage] = useState<"home" | "play">("home"
-    </div>
   )
 }
