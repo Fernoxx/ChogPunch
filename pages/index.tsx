a3d3069374684303a4b0b141f8791a608482f6fa
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-10 03:24:10 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,7 @@
 import { useEffect, useState } from "react"
 
 export default function Home() {
-  const [" | "play">("home")
-  const [farcasterUser, setFarcasterUser] = useState<{ fid: number; username?: string; displayName?: string; pfpUrl?: string } | null>(null)
-
+  const [
   useEffec
       {stage === "play" && 
   )
