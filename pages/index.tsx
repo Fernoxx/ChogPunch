ad80a1d2b64c00ce768a1d2a167dea2f486f9704
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-14 19:27:40 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,2 @@
 // pages/index.tsx
 import { useEffect, useState } from "react"
-import { PlatformerGame } from "../components/PlatformerGame"
-import { motion, AnimatePresence } from "framer-motion"
-import Image from "next/image"
