fd3eb35481a2e45e33a4983174a0d0b3d7cda03b
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-14 19:27:33 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,3 @@ import { useEffect, useState } from "react"
 import { PlatformerGame } from "../components/PlatformerGame"
 import { motion, AnimatePresence } from "framer-motion"
 import Image from "next/image"
-
-export default function Home() {
-  const [stage, setSt
-  useEffect(() => {
-      </AnimatePresence>
