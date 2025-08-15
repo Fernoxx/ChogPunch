e769cf022ff1945792ac6f4c099f3a9a37988871
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-16 04:02:20 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import type { AppProps } from "next/app"
 import { WagmiConfig, createConfig } from "wagmi"
 import { base } from "wagmi/chains"
 import { InjectedConnector } from "wagmi/connectors/injected"
-import { createPublicClient, http } from "viem"
 
 export default function App({ Component, pageProps }: AppProps) {
   const publicClient = createPublicClient({
